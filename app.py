45390632fec74e1b61fd9f72fab513d1c8c75d30
sohelmohammedQT <sohel879879@gmail.com>
2024-09-02 13:23:16 -0400

    set PostgresSQl with Flask

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@localhost:5432/postgres'
+db = SQLAlchemy(app)
 
 @app.route('/api/status', methods=['GET'])
 def status():
